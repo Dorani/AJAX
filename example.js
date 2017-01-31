1d4a1081e71b471681d80fdfa4c7c1bd8af1e839
Seif <seif.dorani@gmail.com>
2017-01-31 13:14:09 -0500

    state changes with requests

--- example.js
+++ example.js
@@ -7,3 +7,12 @@ var ajax = new XMLHttpRequest();
 //----------
 
 //Create a callback function:
+ajax.onreadystatechange = function(){
+  if (ajax.readyState === 4){
+    document.getElementByID('ajax').innerHTML = ajax.responseText;
+  }
+};
+//event is triggered whenever there is a change with an ajax request,
+//we create the callback to respond to that request
+//the above programing sets up a function that runs everytime there is a change
+//in the state in the ajax request
