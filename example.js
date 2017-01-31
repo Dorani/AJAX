42e783f7a43171c4920f2918210946f76bce6d2d
Seif <seif.dorani@gmail.com>
2017-01-31 13:04:57 -0500

    more notes, plus examples of how to get started on new file

---
+++ example.js
@@ -0,0 +1,9 @@
+
+//Create an XMLHTTP request obj:
+var ajax = new XMLHttpRequest();
+//create var ajax which contains a new obj
+//tells the web browser get ready to work with ajax
+
+//----------
+
+//Create a callback function:
