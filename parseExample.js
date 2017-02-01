ba778b91333064edfbcb1594f9001cb82979f05b
Seif <seif.dorani@gmail.com>
2017-02-01 11:31:57 -0500

    notes to examples

--- parseExample.js
+++ parseExample.js
@@ -1,11 +1,11 @@
-var ajax = new XMLHttpRequest();
-ajax.onreadystatechange = function(){
-  if(ajax.readyState === 4){
-    var employees = JSON.parse(ajax.responseText);
+var ajax = new XMLHttpRequest();// request obj, the new var holds the obj that has its on set of prop and methods
+ajax.onreadystatechange = function(){//ready state change event add our callback function to it
+  if(ajax.readyState === 4){// make sure we receive all the json data
+    var employees = JSON.parse(ajax.responseText);//method built into all web browser, string conversion.
   }
 };
-ajax.open('GET', 'data/employees.json');
-ajax.send();
+ajax.open('GET', 'data/employees.json');//open a request, using the get method to load a file filled with json data located in a folder, normally we point to a server side script
+ajax.send();// send the request, the callback function does nothing till we send the request
 
 //json . parse is complete it returns a js object
 //json.parse converted the text into a js array of items that we can manipualte with js
