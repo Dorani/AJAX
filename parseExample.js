ab63f0edef6df0c801074a1f0c0846128941fe2b
Seif <seif.dorani@gmail.com>
2017-02-01 11:42:39 -0500

    loopp to run thru list of employees, created employees var with a list of employees in an array, so we need to go through the array 1 item at a time, retrive employee name and determine if they are in or out of office

--- parseExample.js
+++ parseExample.js
@@ -2,6 +2,10 @@ var ajax = new XMLHttpRequest();// request obj, the new var holds the obj that h
 ajax.onreadystatechange = function(){//ready state change event add our callback function to it
   if(ajax.readyState === 4){// make sure we receive all the json data
     var employees = JSON.parse(ajax.responseText);//method built into all web browser, string conversion.
+    var statusHTML = '<ul class="bulleted">';
+    for (var i = 0; i < employees.length; i++){
+      employees[i]
+    }
   }
 };
 ajax.open('GET', 'data/employees.json');//open a request, using the get method to load a file filled with json data located in a folder, normally we point to a server side script
