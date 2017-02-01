68a2ebe1954aea7b879ec755efa560b736d012bd
Seif <seif.dorani@gmail.com>
2017-02-01 12:10:24 -0500

    jquery basic get request

--- queryajax.js
+++ queryajax.js
@@ -10,3 +10,20 @@ function sendAjax(){
 //easily hide the button as well with jquery method
 
 //makes programing faster and easier.
+
+//find shorthand jquery methods on api
+var data = {
+  firstName: "Dave",
+  lastName: "Thompson"
+};
+var callback = function(response){
+  //do something with response
+}
+$.get(url, data, callback);
+
+// passing values into the get method
+//script might be located at the root of the web server
+//data added to url as a query string, appears at the end of url, data sent to web server so the server can respond appro
+//finally the callback function processes the returned data, works like the js callback
+//but with jquery you dont need to check the xhr ready status, and http status errors
+//calback functions ONLY runs with all things are good!
